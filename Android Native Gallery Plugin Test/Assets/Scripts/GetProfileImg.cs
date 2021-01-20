8594794f4015a5cfa375fdf430ea91a278cddbd8
JeoungKyoungSoo <74169471+JeoungKyoungSoo@users.noreply.github.com>
2021-01-20 11:54:35 +0900

    project fin
    project fin

---
+++ Android Native Gallery Plugin Test/Assets/Scripts/GetProfileImg.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GetProfileImg : MonoBehaviour
+{
+    [SerializeField]
+    private RawImage profileImg;
+
+    private void PickImage(int maxSize)
+    {
+        NativeGallery.Permission permission = NativeGallery.GetImageFromGallery((path) =>
+        {
+            if (path != null)
+            {
+                Texture2D texture = NativeGallery.LoadImageAtPath(path, maxSize);
+                if (texture == null)
+                {
+                    return;
+                }
+                
+                profileImg.texture = texture;
+            }
+        });
+    }
+
+    public void OnClickProfileSwtich()
+    {
+        PickImage(1028);
+    }
+
+}
