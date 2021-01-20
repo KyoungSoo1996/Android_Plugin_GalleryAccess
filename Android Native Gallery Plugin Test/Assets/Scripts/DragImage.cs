5f62f595897f61bcab39c928a58de62b49b5730f
JeoungKyoungSoo <74169471+JeoungKyoungSoo@users.noreply.github.com>
2021-01-21 01:36:55 +0900

    temp
    temp

---
+++ Android Native Gallery Plugin Test/Assets/Scripts/DragImage.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using System.Collections;
+public class DragImage : EventTrigger
+{
+    private bool startDragging;
+    float[] posGroup = { 0, 0 };
+    public override void OnDrag(PointerEventData eventData)
+    {
+        GetComponent<Button>().interactable = false;
+        posGroup = gameObject.GetComponentInParent<GetProfileImg>().MoveImage(Input.mousePosition.x - Screen.width / 2, Input.mousePosition.y - Screen.height / 2 - 125);
+    }
+
+    public override void OnPointerDown(PointerEventData eventData)
+    {
+        startDragging = true;
+    }
+
+    public override void OnPointerUp(PointerEventData eventData)
+    {
+        startDragging = false;
+        StartCoroutine(CoStartButton());
+    }
+
+    private IEnumerator CoStartButton()
+    {
+        yield return new WaitForSeconds(0.3f);
+        GetComponent<Button>().interactable = true;
+        yield break;
+    }
+
+    public void OnClickSave(){
+        StartCoroutine(gameObject.GetComponentInParent<GetProfileImg>().CoSaveImg(GetComponentInParent<GetProfileImg>().profileImg.texture, 0, 0));
+    }
+}
--- Android Native Gallery Plugin Test/Assets/Scripts/GetProfileImg.cs
+++ Android Native Gallery Plugin Test/Assets/Scripts/GetProfileImg.cs
@@ -1,12 +1,62 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.IO;
 using UnityEngine;
 using UnityEngine.UI;
+using System.Collections;
 
 public class GetProfileImg : MonoBehaviour
 {
-    [SerializeField]
-    private RawImage profileImg;
+    public RawImage profileImg;
+    [SerializeField] private Image bakcgroundImg;
+
+    private void Awake()
+    {
+        SwitchImage(false);
+    }
+    private float xValue, yValue;
+
+    public float[] MoveImage(float x, float y)
+    {
+        float xMove = 0.0f, yMove = 0.0f;
+        if (xValue < yValue)
+        {
+            xMove = (x + 50) / 50;
+            profileImg.uvRect = new Rect(Mathf.Clamp(-xMove, 0, 1 - xValue), 0, xValue, yValue);
+        }
+        else if (xValue > yValue)
+        {
+            yMove = (y + 50) / 50;
+            profileImg.uvRect = new Rect(0, Mathf.Clamp(-yMove, 0, 1 - yValue), xValue, yValue);
+        }
+        else
+        {
+            return null;
+        }
+        float[] result = { xMove, yMove };
+        return result;
+    }
+
+    public IEnumerator CoSaveImg(Texture _texture, int x, int y)
+    {
+        yield return new WaitForEndOfFrame();
+        var width  = (int)(128 * (xValue >= 1 ? 1 : xValue));
+        var height = (int)(128 * (yValue >= 1 ? 1 : yValue));
+        Texture2D texture = ScaleTexture((Texture2D)_texture, width, height, x, y);
+
+        Color[] colors = texture.GetPixels(0, 0, width, height);
+        texture.Apply();
+        profileImg.texture = texture;
+        byte[] bytes = texture.EncodeToPNG();
+        File.WriteAllBytes(Application.dataPath + "/" + "test.png", bytes);
+        Debug.Log(Application.dataPath + "/" + "test.png");
+    }
+
+    private void SwitchImage(bool sw)
+    {
+        int value = sw ? 1 : 0;
+        bakcgroundImg.GetComponent<CanvasGroup>().alpha = 1 - value;
+        bakcgroundImg.GetComponent<CanvasGroup>().interactable = !sw;
+        profileImg.GetComponent<CanvasGroup>().alpha = value;
+    }
 
     private void PickImage(int maxSize)
     {
@@ -14,20 +64,42 @@ public class GetProfileImg : MonoBehaviour
         {
             if (path != null)
             {
-                Texture2D texture = NativeGallery.LoadImageAtPath(path, maxSize);
+                Texture2D texture = NativeGallery.LoadImageAtPath(path, maxSize, false);
                 if (texture == null)
                 {
                     return;
                 }
-                
                 profileImg.texture = texture;
+                xValue = texture.texelSize.x;
+                yValue = texture.texelSize.y;
+                if (xValue > yValue) { yValue = yValue / xValue; xValue = 1; }
+                else if (xValue < yValue) { xValue = xValue / yValue; yValue = 1; }
+                else { xValue = 1; yValue = 1; }
+                profileImg.uvRect = new Rect(0, 0, xValue, yValue);
+                SwitchImage(true);
             }
         });
     }
 
     public void OnClickProfileSwtich()
     {
-        PickImage(1028);
+        PickImage(128);
+    }
+
+
+    private Texture2D ScaleTexture(Texture2D source, int targetWidth, int targetHeight, int posX, int posY)
+    {
+        Texture2D result = new Texture2D(targetWidth, targetHeight, source.format, true);
+        Color[] rpixels = result.GetPixels(0);
+        float incX = (1.0f / (float)targetWidth);
+        float incY = (1.0f / (float)targetHeight);
+        for (int px = 0; px < rpixels.Length; px++)
+        {
+            rpixels[px] = source.GetPixelBilinear(incX * ((float)px % targetWidth), incY * ((float)Mathf.Floor(px / targetWidth)));
+        }
+        result.SetPixels(posX, posY, targetWidth, targetHeight, rpixels, 0);
+        result.Apply();
+        return result;
     }
 
 }
